a3359fb92be3fceb6c828a396673a3acf91b4a4c
Tiago Reis <tiagovrtreis@gmail.com>
2025-02-04 17:59:16 +0000

    fix: force reinstall
    Signed-off-by: Tiago Reis <tiagovrtreis@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 if [ -z "$INPUT_POETRY_VERSION" ]; then
     echo "Poetry version set to $INPUT_POETRY_VERSION, overriding default version"
-    pip install poetry==$INPUT_POETRY_VERSION
+    pip install --force-reinstall poetry=="$INPUT_POETRY_VERSION"
 fi
 poetry --directory /diff_poetry_lock run python3 /diff_poetry_lock/run_poetry.py
