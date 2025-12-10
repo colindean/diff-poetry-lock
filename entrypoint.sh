fe179d150946deedbe5ac282ce40bbb4b45f1111
Lakshmi.Narayanan <lakshmi.narayanan@target.com>
2025-12-10 16:04:08 -0600

    Corrects variable emptiness logic in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ -z "$INPUT_POETRY_VERSION" ]; then
-    echo "Poetry version set to $INPUT_POETRY_VERSION, overriding default version"
+if [ -n "$INPUT_POETRY_VERSION" ]; then
+    echo "Poetry version set to [$INPUT_POETRY_VERSION], overriding default version"
     pip install --force-reinstall poetry=="$INPUT_POETRY_VERSION"
 fi
 poetry --directory /src run python3 -m diff_poetry_lock.run_poetry
